f83227145339ad154bcf0471a1362cd56a924e92
Stef Van Nieuwenhove <stef.vannieuwenhove@telenet.be>
2022-02-09 19:29:51 +0100

    update feb

--- html/script.js
+++ html/script.js
@@ -7,23 +7,27 @@ function renderPlaats() {
         option.setAttribute('label', value);
         option.innerText = value;
         document.getElementById('2022').appendChild(option);
-        option.addEventListener('click', document.getElementById('title').innerHTML = `${value}`);
+        option.addEventListener('click', console.log(value));
     })
 }
 
 function toonTitel(value) {
     console.log(value);
-    document.getElementById('title').innerHTML = '';
-    document.getElementById('title').innerHTML = `${value}`;
+    //document.getElementById('title').innerHTML = '';
+    //document.getElementById('title').innerHTML = `${value}`;
 }
 
 function toonFotos(plaats) {
     document.getElementById('title').innerHTML = plaats;
+    
 }
 
 
 function init() {
      renderPlaats();
+     const plaats = document.getElementById('2022').value;
+     document.getElementById('2022').addEventListener('change', () => toonFotos(plaats))
+     
 }
 
 onload = init;
