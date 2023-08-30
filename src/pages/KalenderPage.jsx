2ff9ce8bd33062b93557832e6724c11a0f5af557
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-08-30 17:26:17 +0200

    kalender sep okt 23

--- src/pages/KalenderPage.jsx
+++ src/pages/KalenderPage.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Table,
   TableBody,
@@ -13,54 +13,59 @@ import {
   TableCell,
   Container,
   Button,
-} from "@mui/material";
-import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+} from '@mui/material';
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 
 const kalender = [
   {
-    datum: "02/07/2023",
-    uur: "9:00",
-    plaats: "Teralfene",
+    datum: '03/09/2023',
+    uur: '8:30',
+    plaats: 'Teralfene',
   },
   {
-    datum: "09/07/2023",
-    uur: "8:30",
-    plaats: "Wandelzoektocht",
+    datum: '04/09/2023',
+    uur: '11:30',
+    plaats: 'BBQ en gere bolling t.v.v. de clubkas - Natekeshof',
   },
   {
-    datum: "16/07/2023",
-    uur: "9:00",
-    plaats: "Wandelzoektocht",
+    datum: '10/09/2023',
+    uur: '9:00',
+    plaats: 'Teralfene',
   },
   {
-    datum: "23/07/2023",
-    uur: "8:30",
-    plaats: "Welle meersen",
+    datum: '17/09/2023',
+    uur: '8:30',
+    plaats: 'O.L.V. Lombeek',
   },
   {
-    datum: "30/07/2023",
-    uur: "9:00",
-    plaats: "Teralfene",
+    datum: '24/09/2023',
+    uur: '9:00',
+    plaats: 'Teralfene',
   },
   {
-    datum: "06/08/2023",
-    uur: "8:30",
-    plaats: "Liedekerke bos",
+    datum: '01/10/2023',
+    uur: '8:30',
+    plaats: 'Leerbeek',
   },
   {
-    datum: "13/08/2023",
-    uur: "9:00",
-    plaats: "Teralfene",
+    datum: '08/10/2023',
+    uur: '9:00',
+    plaats: 'Teralfene',
   },
   {
-    datum: "20/08/2023",
-    uur: "8:30",
-    plaats: "Affligem - abdij",
+    datum: '15/10/2023',
+    uur: '8:30',
+    plaats: 'Gaasbeek',
   },
   {
-    datum: "27/08/2023",
-    uur: "9:00",
-    plaats: "Teralfene",
+    datum: '22/20/2023',
+    uur: '9:00',
+    plaats: 'Teralfene',
+  },
+  {
+    datum: '29/10/2023',
+    uur: '8:30',
+    plaats: 'Waarbeke',
   },
 ];
 
@@ -74,30 +79,30 @@ const KalenderPage = () => {
 
   return (
     <>
-      <Container maxWidth="md">
+      <Container maxWidth='md'>
         <TableContainer component={Paper} elevation={10}>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell colSpan={3} sx={{ textAlign: "center" }}>
-                  <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-                    Kalender Juli en Augustus 2023
+                <TableCell colSpan={3} sx={{ textAlign: 'center' }}>
+                  <Typography variant='h4' sx={{ fontWeight: 'bold' }}>
+                    Kalender September en Oktober 2023
                   </Typography>
                 </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell>
-                  <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
                     Datum
                   </Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
                     Uur
                   </Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                  <Typography variant='h5' sx={{ fontWeight: 'bold' }}>
                     Plaats
                   </Typography>
                 </TableCell>
@@ -105,39 +110,25 @@ const KalenderPage = () => {
             </TableHead>
             <TableBody>
               {data.map((item) => (
-                <TableRow key={item.datum} sx={{ textAlign: "center" }}>
+                <TableRow key={item.datum} sx={{ textAlign: 'center' }}>
                   <TableCell>
-                    <Typography variant="h6">{item.datum}</Typography>
+                    <Typography variant='h6'>{item.datum}</Typography>
                   </TableCell>
                   <TableCell>
-                    <Typography variant="h6">{item.uur}</Typography>
+                    <Typography variant='h6'>{item.uur}</Typography>
                   </TableCell>
-                  <TableCell sx={{ width: "40%" }}>
-                    <Typography variant="h6">{item.plaats}</Typography>
+                  <TableCell sx={{ width: '40%' }}>
+                    <Typography variant='h6'>{item.plaats}</Typography>
                   </TableCell>
                 </TableRow>
               ))}
-              <TableRow sx={{ textAlign: "center" }}>
-                <TableCell>
-                  <Typography variant="h6">04/09/2023</Typography>
-                </TableCell>
-                <TableCell
-                  rowSpan={2}
-                  sx={{ display: "flex", width: "100%", flexWrap: "none" }}
-                >
-                  <Typography variant="h6">
-                    Maandag Teralfene kermis - Barbeque en gerre bolling
-                    Natekenshof
-                  </Typography>
-                </TableCell>
-              </TableRow>
             </TableBody>
             <TableFooter>
               <TableRow>
-                <TableCell colSpan={3} sx={{ textAlign: "center" }}>
+                <TableCell colSpan={3} sx={{ textAlign: 'center' }}>
                   <Typography
-                    variant="h6"
-                    sx={{ fontWeight: "bold", color: "black" }}
+                    variant='h6'
+                    sx={{ fontWeight: 'bold', color: 'black' }}
                   >
                     Voor de verplaatsing, beter een auto te veel dan één te
                     weinig!
@@ -145,15 +136,15 @@ const KalenderPage = () => {
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell colSpan={3} sx={{ textAlign: "center" }}>
+                <TableCell colSpan={3} sx={{ textAlign: 'center' }}>
                   <Button
-                    component="a"
-                    href="/pdf/Kalender_JuliAugustus_2023.pdf"
-                    download="Kalender_juliAugustus_2023.pdf"
-                    variant="contained"
-                    size="lg"
+                    component='a'
+                    href='/pdf/Kalender_SepOkt_2023.pdf'
+                    download='Kalender_SepOkt_2023.pdf'
+                    variant='contained'
+                    size='lg'
                     OnClick={handleClick}
-                    color="error"
+                    color='error'
                     endIcon={<ArrowCircleDownIcon />}
                   >
                     <Typography>Download de kalender</Typography>
